683948fb962d6a39d7bb6d08692d515e92798b71
Rabyaessanii <rabbiaessani52@gmail.com>
2024-09-03 19:23:43 +0500

    Blog Crud API endpoints created

--- CollegeChemistry/CollegeChemistryAPI/Controllers/BlogsController.cs
+++ CollegeChemistry/CollegeChemistryAPI/Controllers/BlogsController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 using CollegeChemistryLibrary.Interfaces;
+using CollegeChemistryLibrary.Entities;
+using System.Reflection.Metadata;
 
 namespace CollegeChemistryAPI.Controllers
 {
@@ -15,7 +17,7 @@ namespace CollegeChemistryAPI.Controllers
         private readonly ILogger<BlogsController> _logger;
         private Interface_Blogs _college_blogs;
 
-        public BlogsController(Interface_Blogs college_blogs ,ILogger<BlogsController> logger)
+        public BlogsController(Interface_Blogs college_blogs, ILogger<BlogsController> logger)
         {
             _logger = logger;
             _college_blogs = college_blogs;
@@ -26,7 +28,7 @@ namespace CollegeChemistryAPI.Controllers
         {
             try
             {
-               var result= _college_blogs.GetAllBlogs();
+                var result = _college_blogs.GetAllBlogs();
                 return Ok(result);
 
             }
@@ -35,5 +37,83 @@ namespace CollegeChemistryAPI.Controllers
                 return BadRequest("GetBlogs Exception Error: " + xc);
             }
         }
+
+        [HttpGet("{id}")]
+        public ActionResult GetBlogsById(int id)
+        {
+            try
+            {
+                var result = _college_blogs.GetBlogById(id);
+                return Ok(result);
+
+            }
+            catch (Exception xc)
+            {
+                return BadRequest("GetBlogs by Id Exception Error: " + xc);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult PostBlogs([FromBody] Blogs blog)
+        {
+            try
+            {
+                var result = _college_blogs.AddBlog(blog);
+                if (result == 0)
+                {
+                    return StatusCode(500, "An error occurred while adding the blog.");
+                }
+                return Ok(result);
+
+            }
+            catch (Exception xc)
+            {
+                return BadRequest("AddBlogs  Exception Error: " + xc);
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteBlog([FromBody] Blogs blog)
+        {
+            try
+            {
+                var result = _college_blogs.DeleteBlog(blog);
+                if (result)
+                {
+                    return Ok(new { message = "Blog deleted successfully." });
+                }
+                else
+                {
+                    return NotFound(new { message = "Blog not found or could not be deleted." });
+                }
+            }
+            catch (Exception xc)
+            {
+                return BadRequest("DeleteBlog Exception Error: " + xc);
+            }
+        }
+
+        [HttpPatch]
+        public ActionResult UpdateBlog([FromBody] Blogs blog)
+        {
+            try
+            {
+                bool result = _college_blogs.UpdateBlog(blog);
+                if (result)
+                {
+                    return Ok(new { message = "Blog updated successfully." });
+                }
+                else
+                {
+                    return NotFound(new { message = "Blog not found or could not be updated." });
+                }
+            }
+            catch (Exception xc)
+            {
+                return BadRequest("UpdateBlog  Exception Error: " + xc);
+            }
+
+        }
     }
+
 }
--- CollegeChemistry/CollegeChemistryLibrary/CollegeChemistryBlogsLibrary.cs
+++ CollegeChemistry/CollegeChemistryLibrary/CollegeChemistryBlogsLibrary.cs
@@ -1,9 +1,12 @@
-﻿using Newtonsoft.Json;
+﻿using CollegeChemistryLibrary.Entities;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace CollegeChemistryLibrary
 {
@@ -14,6 +17,76 @@ namespace CollegeChemistryLibrary
         public string GetAllBlogs( string baseUrl, string ApiKey, string ApiHeader)
         {
             return _colchemWebApi.GetRecords(baseUrl + col_chem_api_controller_name + "/allblogs/", ApiKey, ApiHeader);
+        }
+
+        public string GetBlogById(string baseUrl,string ApiKey, string ApiHeader, int blogId)
+        {
+            string url = $"{baseUrl}{col_chem_api_controller_name}/{blogId}";
+            return _colchemWebApi.GetRecords(url , ApiKey, ApiHeader);
+        }
+
+        public int AddBlog(string baseUrl, string ApiKey, string ApiHeader, Blogs blog)
+        {
+            string blogData = JsonSerializer.Serialize(blog, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            });
+
+            
+            string url = $"{baseUrl}{col_chem_api_controller_name}/postblog";
+
+
+            string response = _colchemWebApi.CommitPostActionWithReturn(blogData, url, ApiKey, ApiHeader);
+            if (int.TryParse(response, out int blogId))
+            {
+                return blogId;
+            }
+            else
+            {
+                // Handle the case where the response is not a valid integer (e.g., log an error or throw an exception)
+                throw new InvalidOperationException("The API response did not return a valid blog ID.");
+            }
+        }
+
+        public bool DeleteBlog(string baseUrl, string ApiKey, string ApiHeader, Blogs blog)
+        {
+            string blogData = JsonSerializer.Serialize(blog, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            });
+
+
+            string url = $"{baseUrl}{col_chem_api_controller_name}";
+
+            // Send the request to the delete endpoint
+            string response = _colchemWebApi.CommitPostActionWithReturn(blogData, url, ApiKey, ApiHeader);
+
+            // Determine if the deletion was successful
+            if (!string.IsNullOrEmpty(response) && response.Contains("success", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdateBlog(string baseUrl, string ApiKey, string ApiHeader, Blogs blog)
+        {
+            string blogData = JsonSerializer.Serialize(blog, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            });
+
+            string url = $"{baseUrl}{col_chem_api_controller_name}";
+            string response = _colchemWebApi.CommitPostActionWithReturn(blogData, url, ApiKey, ApiHeader);
+
+            if (!string.IsNullOrEmpty(response) && response.Contains("success", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
